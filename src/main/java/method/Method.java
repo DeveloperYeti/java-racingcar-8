1c4ed0da328143a549d8256b441d79787b70abfc
DeveloperYeti <hoseok9823@gmail.com>
2025-10-21 21:20:12 +0900

    0,1 범위 내에서 랜덤 값을 추출하는 Move 메서드 작성 (앞 멈춤 움직임)

--- src/main/java/method/Method.java
+++ src/main/java/method/Method.java
@@ -2,13 +2,17 @@ package method;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
 public class Method {
 
     public static void Input() {
 
+        return;
     }
-    public static void Move() {
-
+    //
+    public static int Move(final List<Integer> Act) {
+        return Randoms.pickNumberInList(Act);
     }
 
 
