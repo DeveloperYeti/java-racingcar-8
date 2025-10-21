78628e622dfef3999692858adf422b9537482fa7
DeveloperYeti <hoseok9823@gmail.com>
2025-10-21 20:48:30 +0900

    Method 폴더 생성 및 파일 생성 Input Move 메소드 생성.

---
+++ src/main/java/method/Method.java
@@ -0,0 +1,15 @@
+package method;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Method {
+
+    public static void Input() {
+
+    }
+    public static void Move() {
+
+    }
+
+
+}
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,12 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // 커밋 테스트.
+
     }
 }
