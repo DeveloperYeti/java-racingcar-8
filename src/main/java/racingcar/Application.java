7ac436113ba6407dd7541e7b0f6fd89dc7a180b2
DeveloperYeti <hoseok9823@gmail.com>
2025-10-21 21:14:55 +0900

    final 사용 정수형 리스트 선언.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,11 +2,14 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        // 커밋 테스트.
+        // 랜덤한 0~9 사이의 값. List 사용.
+        final List<Integer> numbers = List.of(0,1,2,3,4,5,6,7,8,9);
 
     }
 }
