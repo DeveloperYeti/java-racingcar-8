f337869ad82331e10b1c2d3af2a8488af3735d52
DeveloperYeti <hoseok9823@gmail.com>
2025-10-22 17:42:01 +0900

    final 상수 0 ,1 선언 (멈춤 , 나아감 움직임)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class Application {
     final List<Integer> numbers = List.of(0,1,2,3,4,5,6,7,8,9);
     final List<Integer> Act = List.of(0,1);
+    final int start = 0;
+    final int end = 1;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
