54477bae9c339c9d0bde970d987b254f62053222
DeveloperYeti <hoseok9823@gmail.com>
2025-10-22 17:42:32 +0900

    Move 메서드 List-> 값에서 Range 값을 사용하는 함수로 변경. Randoms 참조.

--- src/main/java/method/Method.java
+++ src/main/java/method/Method.java
@@ -2,17 +2,34 @@ package method;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class Method {
+    public static List<String> inputName() {
+        System.out.println("시도할 횟수");
+        String str = Console.readLine();
+        int trychance = Integer.parseInt(str);
 
-    public static void Input() {
+        if(trychance>=0 && trychance < 5){
+            List<String> name = new ArrayList<String>();
 
-        return;
+            for(int i=0;  i<=trychance ;i++){
+                String inputName = Console.readLine();
+                name.add(i,inputName);
+            }
+
+            return name;
+        }
+        else {
+            throw new IllegalArgumentException("잘못된 값 입력.");
+        }
     }
-    //
-    public static int Move(final List<Integer> Act) {
-        return Randoms.pickNumberInList(Act);
+
+    //요구 사항에서 Range 함수를 사용하라고 해서 함수 사용 및 값 반환.
+    public static int Move(final int start , final int end) {
+        return Randoms.pickNumberInRange(start,end);
     }
 
 
