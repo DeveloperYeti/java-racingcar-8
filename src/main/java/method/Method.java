c728ccc9f7075d553bc85c1925778f3dfb83fede
DeveloperYeti <hoseok9823@gmail.com>
2025-10-24 15:43:20 +0900

    입력된 값에 대해서 ,를 구분자로 사용하여 반환된 리스트를 return 값으로 사용.

--- src/main/java/method/Method.java
+++ src/main/java/method/Method.java
@@ -2,48 +2,49 @@ package method;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Method {
-
-    public  List<String> inputName(){
+    // 입력된 값에 대해서 ,를 구분자로 사용하여 반환된 리스트를 return 값으로 사용.
+    public static List<String> inputName(){
         List<String > myList = new ArrayList<>();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String inputName = Console.readLine();
         String[] inputNameArr = inputName.split(",");
-
-
-
-
-        return ;
+        for(String name : inputNameArr)
+            myList.add(name.trim());
+        return myList;
     }
+
     // 총 시도할 횟수 표시
     public static List<String> inputChance(int nameNumbers) {
+        inputName();
         System.out.println("시도할 횟수");
         String str = Console.readLine();
         int tryChance = Integer.parseInt(str);
 
         if(tryChance>=0 && tryChance < 5){
-            for (int i=0; i<nameNumbers; i++){
-                List<String> name = new ArrayList<String>();
-                for(int k=0; k<tryChance; k++){
-                    name.add(k,"-");
-                }
-                return  name;
-            }
-
+            Repeat(nameNumbers);
         }
         else {
             throw new IllegalArgumentException("잘못된 값 입력.");
         }
+        return Repeat();
+    }
+    // 각각의 name List를 생성하고 그에 맞
+
+    public static List<List<String>> Repeat(int nameNumbers) {
+        List<List<String>> names = new ArrayList<>();
+        for (int i = 0; i < nameNumbers; i++) {
+            List<String> name = new ArrayList<String>();
+            names.add(name);
+        }
+        return names;
     }
 
     //요구 사항에서 Range 함수를 사용하라고 해서 함수 사용 및 값 반환.
     public static int Move(final int start , final int end) {
         return Randoms.pickNumberInRange(start,end);
     }
-
-
 }
